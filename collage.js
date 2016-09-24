803e670da067bb4a8d40bd4ffd22b08d6f6bb90f
Alexandr Drozd <alexandr1drozd@gmail.com>
2016-09-24 19:04:25 +0300

    Update collage.js

--- collage.js
+++ collage.js
@@ -4,45 +4,49 @@
 *	Year: 2016
 */
 (function($){
-	$.fn.colage = function(p){
-		Array.prototype.sum = function(){
-			return this.reduce(function(a,b){
-				return  +b+a;
-			}, 0);
-		}
-		$(this).each(function(){
-			var b = $(this), it = b.find(p.it || 'img'), pw = p.width || b.get(0).clientWidth,
-			pc = pw/100, avh = p.AverageHeight || 175, m = p.margin || 3;
-			b.css({'width' : pw+'px','clear' : ''});
-			it.css({'display': 'block','float': 'left'});
-			var ws = [], rs = [], rn = 0, id = 0;
-				for(var i = 0; i < it.length; i++) ws.push(Math.round(it[i].width/it[i].height*avh));
-			while(rn < Math.ceil(ws.sum()/pw)){
-				rs[rn] = [];
-					while(id < ws.length && rs[rn].sum()+(rs[rn].length*m) < pw+m){
-						rs[rn].push(ws[id]);
-						id++;
-						if(id == ws.length-1){
-							rs[rn].push(ws[id]);
-							rn++;
-							break;
-						}
+	$.fn.collage = function(opts){
+		this.each(function(){
+			var opts = opts || {
+				maxHeight: 200,
+				margin: 3
+			}, d = {
+				width: this.clientWidth,
+				img: $(this).find('img')
+			}, widths = [], s = [0, []], r = [], l = 0, i = 0;
+			d.img.each(function(c, e){
+				var width = Math.round(e.naturalWidth/e.naturalHeight*opts.maxHeight);
+				widths.push(width);
+				s[0] = s[0]+width;
+			});
+			while(l < Math.ceil(s[0]/d.width)){
+				r[l] = [];
+				s[1][l] = 0;
+				while(i < widths.length && s[1][l]+(r[l].length*opts.margin)<d.width+opts.margin){
+					r[l].push(widths[i]);
+					s[1][l] = s[1][l]+widths[i];
+					i++;
+					if(i == widths.length-1){
+						r[l].push(widths[i]);
+						s[1][l] = s[1][l]+widths[i];
+						l++;
+						break;
 					}
-				rn++;
+				}
+				l++;
 			}
-			id = 0;
-			for(var i = 0; i < rs.length; i++){
-				try{
-					for(var j = 0; j < rs[i].length; j++){
-						it[id].style.width = rs[i][j]*((pw-((rs[i].length-1)*m))/rs[i].sum())+'px';
-						it[id].style.height = ((pw-((rs[i].length-1)*m))/rs[i].sum())*avh+'px';
-						if(j < rs[i].length-1) it[id].style.marginRight = m+'px';
-						if(i < rs.length-1) it[id].style.marginBottom = m+'px';
-						id++;
-					}
-				} catch(e){}
+			e = 0;
+			for(var i = 0; i < r.length; i++){
+				for(var j = 0; j < r[i].length; j++){
+					var k = (d.width-((r[i].length-1)*opts.margin))/s[1][i], _s = {
+						width: r[i][j]*k+'px',
+						height: k*opts.maxHeight+'px'
+					};
+					if(j < r[i].length-1) _s.marginRight = opts.margin+'px';
+					if(i < r.length-1) _s.marginBottom = opts.margin+'px';
+					d.img.eq(e).css(_s);
+					e++;
+				}
 			}
 		});
-		delete Array.prototype.sum;
 	};
 })(jQuery);
